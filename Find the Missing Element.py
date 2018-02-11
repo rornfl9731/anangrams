ee993809fe3014f1339b35c86375a63554bea2c4
Kim Jin Woo <inx0930@gmail.com>
2018-02-12 00:48:47 +0900

    commenti

---
+++ Find the Missing Element.py
@@ -0,0 +1,50 @@
+'''
+Find the Missing Element
+Problem
+Consider an array of non-negative integers. A second array is formed by shuffling the elements of the first array and deleting a random element. Given these two arrays, find which element is missing in the second array.
+
+Here is an example input, the first array is shuffled and the number 5 is removed to construct the second array.
+
+Input:
+
+finder([1,2,3,4,5,6,7],[3,7,2,1,4,6])
+
+Output:
+
+5 is the missing number
+'''
+
+def finder(arr1,arr2):
+    cur={}
+
+    for i in arr1:
+        if i in cur:
+            cur[i] -=1
+        else:
+            cur[i] = 1
+
+
+    for i in arr2:
+        if i in cur:
+            cur[i] -= 1
+
+
+    for i in cur:
+        if cur[i] !=0:
+            return i
+
+
+from nose.tools import assert_equal
+
+
+class TestFinder(object):
+    def test(self, sol):
+        assert_equal(sol([5, 5, 7, 7], [5, 7, 7]), 5)
+        assert_equal(sol([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6]), 5)
+        assert_equal(sol([9, 8, 7, 6, 5, 4, 3, 2, 1], [9, 8, 7, 5, 4, 3, 2, 1]), 6)
+        print('ALL TEST CASES PASSED')
+
+
+# Run test
+t = TestFinder()
+t.test(finder)
